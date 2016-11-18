da23d906d8770dc4d46b0ecc9a11360771c03b07
NicolasBouhours <nico53960@hotmail.fr>
2016-11-18 23:02:59 +0100

    STEP 45 - Directive Property Binding

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -1,16 +1,16 @@
-import { Directive, ElementRef, Renderer, HostListener, HostBinding } from '@angular/core';
+import { Directive, ElementRef, Renderer, HostListener, HostBinding, Input, OnInit } from '@angular/core';
 
 @Directive({
-  selector: '[dirHighlight]'
+  selector: '[highlight]'
 })
 export class HighlightDirective {
 
   @HostListener('mouseenter') mouseover() {
-    this.backgroundColor = 'green';
+    this.backgroundColor = this.highlightColor;
   }
 
   @HostListener('mouseleave') mouseleave() {
-    this.backgroundColor = 'white';
+    this.backgroundColor = this.defaultColor;
   }
 
   @HostListener('click', ['$event']) onclick(event) {
@@ -21,11 +21,19 @@ export class HighlightDirective {
   @HostBinding('style.backgroundColor') get setColor() {
     return this.backgroundColor;
   }
-  private backgroundColor = 'white';
+
+  @Input() defaultColor = 'white';
+  @Input('highlight') highlightColor = 'green';
+  private backgroundColor: string;
 
   constructor() {
 
   }  
+
+  ngOnInit() {
+    this.backgroundColor = this.defaultColor;
+  }
+
   //constructor(private elementRef: ElementRef, private renderer: Renderer) {
      //this.elementRef.nativeElement.style.backgroundColor = 'green' // PAS UNE BONNE PRATIQUE
      //this.renderer.setElementStyle(this.elementRef.nativeElement, 'background-color', 'green')    // POSSIBLE AUSSI PRATIQUE CORRECTE
